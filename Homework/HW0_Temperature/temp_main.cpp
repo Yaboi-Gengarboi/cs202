828da7ad65bb85e436016c774582774ea7bae8ed
Justyn Durnford <jpdurnford@alaska.edu>
2020-01-15 19:40:35 -0800

    Began writing function cpp_ftoc in temp_main.cpp

--- Homework/HW0_Temperature/temp_main.cpp
+++ Homework/HW0_Temperature/temp_main.cpp
@@ -3,6 +3,16 @@
 //Created on 1/15/2020
 //Last Updated on 1/15/2020
 
+//Takes the given Cstring, cstr, which
+//represents a temperature in fFahrenheit
+//and converts it to celcius.
+double cpp_ftoc(const char* cstr)
+{
+	double return_value = 0.0;
+
+	return return_value;
+}
+
 int main()
 {
 	return 0;
