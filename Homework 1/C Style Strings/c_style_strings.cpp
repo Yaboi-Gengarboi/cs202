66ab782bd883785c2b1a9783aa3cf5a644d6a841
Justyn Durnford <jpdurnford@alaska.edu>
2020-01-23 10:24:54 -0800

    Completed method cstr_size in c_style_strings

--- Homework 1/C Style Strings/c_style_strings.cpp
+++ Homework 1/C Style Strings/c_style_strings.cpp
@@ -2,9 +2,37 @@
 //Justyn P. Durnford
 //Created on 1/23/2020
 //Last Updated on 1/23/2019
-//
+//https://github.com/Yaboi-Gengarboi/cs202/tree/master/Homework%201
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+//Returns the length of the given C-string.
+unsigned int cstr_size(const char* cstr)
+{
+	unsigned int size = 0;
+
+	//The end of a C-string is denoted by the null character, '\0'.
+	//By treating the const char* as a const char[] (which it really is),
+	//We can go through each individual character until we find the null
+	//character and count the characters.
+	while (cstr[size] != '\0')
+		++size;
+
+	return size;
+}
+
+void test()
+{
+	const char* str1 = "Hello, world"; //Size: 12
+	const char* str2 = ""; //Size: 0
+	cout << cstr_size(str1) << endl;
+	cout << cstr_size(str2) << endl;
+}
 
 int main()
 {
+	test();
 	return 0;
 }
