d99c3c1d04a61b900104447d79043d0b93e30db7
Justyn Durnford <jpdurnford@alaska.edu>
2020-01-30 09:26:29 -0800

    Added Hitbox_Circle struct in Homework 2: Linked List with shared_ptr

--- Homework 1/C Style Strings/c_style_strings.cpp
+++ Homework 1/C Style Strings/c_style_strings.cpp
@@ -40,19 +40,54 @@ bool str_cmp(const char* cstr1, const char* cstr2)
 	return false;
 }
 
+//Dynamically allocates a substring of the given C-string on the heap. Calls
+//the str_size method to ensure enough memory is allocated.
+//IMPORTANT: If this method is used you MUST call delete[] on the pointer
+//when it is no longer used, or assign it to a smart pointer. Failure to
+//do so will result in a memory leak.
+char* sub_str(const char* cstr, unsigned int begin, unsigned int end)
+{
+	//If end is greater than the size of cstr, we will just reassign end
+	//to the size of cstr.
+	if (end > str_size(cstr))
+		end = str_size(cstr);
+
+	//If begin is greater than end, we will just return a copy of cstr.
+	if (begin > end)
+		return (char*)cstr;
+
+	//Creates a C-string on the heap with size equal to that of the
+	//range of characters of cstr, end - begin.
+	unsigned int size = end - begin;
+	cout << size << endl;
+	char* newcstr = new char[size];
+	cout << str_size(newcstr) << endl;
+
+	//Loops through and copies each character of cstr to newcstr
+	//within the range [begin, end).
+	for (unsigned int i = begin; i < end; ++i)
+	{
+		cout << cstr[i] << endl;
+		newcstr[i] = cstr[i];
+		cout << newcstr[i] << endl;
+	}
+
+	return newcstr;
+}
+
 //Dynamically allocates a copy of the given C-string on the heap. Calls
 //the str_size method to ensure enough memory is allocated.
 //IMPORTANT: If this method is used you MUST call delete[] on the pointer
 //when it is no longer used, or assign it to a smart pointer. Failure to
 //do so will result in a memory leak.
-const char* str_dup(const char* cstr)
+char* str_dup(const char* cstr)
 {
 	//Creates a C-string on the heap with size equal to that of the
 	//given C-string, cstr.
 	char* newcstr = new char[str_size(cstr)];
 
 	//Loops through and copies each character of cstr to newcstr.
-	for (unsigned int i = 0; i < str_size(newcstr) - 1; ++i)
+	for (unsigned int i = 0; i < str_size(cstr); ++i)
 		newcstr[i] = cstr[i];
 
 	return newcstr;
@@ -62,24 +97,25 @@ const char* str_dup(const char* cstr)
 //cannot be found, this method will return -1.
 int find_x(const char* cstr, const char* x)
 {
-	for (unsigned int i = 0; i < str_size(cstr) - str_size(x); ++i)
-	{
-		for (unsigned int p = str_size(cstr) - str_size(x); p < str_size(x); ++p)
-		{
-			
-		}
-	}
+	
 
 	return -1;
 }
 
-void test()
-{
-	cout << find_x("Hello", "Hell") << endl;
-}
-
 int main()
 {
-	test();
+	//cout << find_x("Hello", "Hell") << endl;
+	char* cstr = str_dup("Hello");
+	cout << cstr << endl;
+
+	const char* cstr2 = "Hello";
+	char* cstr3 = sub_str(cstr2, 0, 5);
+	cout << cstr3 << endl;
+
+	delete[] cstr;
+	delete[] cstr3;
+	cstr = nullptr;
+	cstr3 = nullptr;
+
 	return 0;
 }
---
+++ Homework 2/Linked List with shared_ptr/Hitbox_Circle.hpp
@@ -0,0 +1,33 @@
+//Hitbox_Circle.hpp
+//Justyn P. Durnford
+//Created on 1/30/2020
+//Last Updated on 1/30/2020
+//
+
+#ifndef HITBOX_CIRCLE_HPP
+#define HITBOX_CIRCLE_HPP
+
+struct Hitbox_Circle
+{
+	//Offsets of Hitbox origin
+	int x_off = 0;
+	int y_off = 0;
+	int z_off = 0;
+
+	//Radius of Hitbox
+	int rad = 0;
+
+	//Damage
+	double damage = 0.0;
+
+	//Base Knockback
+	int bkb = 0;
+
+	//Knockback Scaling
+	int kbs = 0;
+
+	//Angle (In Degrees)
+	double angle = 0.0;
+};
+
+#endif //#ifndef HITBOX_CIRCLE_HPP
