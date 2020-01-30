c0209c7c89f7f477c2beaf3b1bb32fb40144131d
Justyn Durnford <jpdurnford@alaska.edu>
2020-01-30 09:46:28 -0800

    --Included the Catch2 library and other headers in Linked_List_main.cpp --Added and tested method printHitboxCircle

--- Homework 2/Linked List with shared_ptr/Linked_List_main.cpp
+++ Homework 2/Linked List with shared_ptr/Linked_List_main.cpp
@@ -4,7 +4,30 @@
 //Last Updated on 1/30/2020
 //
 
+//External Libraries
+#include <catch2/catch.hpp>
+
+//User-created headers
+#include "Hitbox_Circle.hpp"
+
+//Standard Library
+#include <iostream>
+using std::cout;
+using std::endl;
+
+void printHitboxCircle(const Hitbox_Circle& hb)
+{
+	cout << "Offsets: < " << hb.x_off << ", " << hb.y_off << ", " << hb.z_off << " >" << endl;
+	cout << "Radius: " << hb.rad << endl;
+	cout << "Damage: " << hb.damage << endl;
+	cout << "Base Knockback: " << hb.bkb << endl;
+	cout << "Knockback Scaling: " << hb.kbs << endl;
+	cout << "Angle: " << hb.angle << endl;
+}
+
 int main()
 {
+	Hitbox_Circle hb{ 0, 0, 0, 10, 8.0, 40, 120, 50 };
+	printHitboxCircle(hb);
 	return 0;
 }
