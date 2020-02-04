5eb9054ff2b7f3288ac2e777fa70f3bfb1d99b61
Justyn Durnford <jpdurnford@alaska.edu>
2020-02-04 09:37:43 -0800

    Added and tested method writeToFileN() in Lab4_main.cpp

--- Lab4/Lab4_main.cpp
+++ Lab4/Lab4_main.cpp
@@ -17,6 +17,7 @@ using std::endl;
 #include <fstream>
 using std::ifstream;
 using std::ofstream;
+using std::ios_base;
 
 //Gets every line from the txt file tied to the ifstream
 //and prints it to the terminal window.
@@ -35,11 +36,24 @@ unsigned int readFile(ifstream& fin)
 	return index;
 }
 
+//Writes the line to the txt file tied to the ofstream n
+//times.
+void writeToFileN(ofstream& fout, const string& line, const unsigned int& n)
+{
+	for (unsigned int i = 0; i < n; ++i)
+		fout << line << endl;
+}
+
 int main()
 {
 	ifstream fin("Lab4txt.txt");
+	unsigned int begin_index = 0;
+
 	if (fin)
-		cout << readFile(fin) << endl;
+		begin_index = readFile(fin);
+
+	ofstream fout("Lab4txt.txt", ios_base::app);
+	writeToFileN(fout, "NEWLINE", 5);
 
 	return 0;
 }
