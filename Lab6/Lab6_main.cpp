f4c34d4674cf235f4735c3ac69c541421fa83266
Justyn Durnford <jpdurnford@alaska.edu>
2020-02-13 10:39:25 -0800

    Started Lab6

---
+++ Lab6/Lab6_main.cpp
@@ -0,0 +1,5 @@
+//Lab6_main.cpp
+//Justyn Durnford
+//Created on 2/13/2020
+//Last Updated on 2/13/2020
+//
---
+++ Lab6/names.hpp
@@ -0,0 +1,5 @@
+//names.hpp
+//Justyn Durnford
+//Created on 2/13/2020
+//Last Updated on 2/13/2020
+//
