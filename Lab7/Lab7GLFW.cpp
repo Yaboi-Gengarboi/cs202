e2c20414ca3164aa8a4ffc7f49193ea1f94183f9
Justyn Durnford <jpdurnford@alaska.edu>
2020-02-20 11:18:01 -0800

    --Added a triangle to the window (so prettyyyyyyyyyy!) --Added GLFWLab_Refractor.cpp that we will work on later

--- Lab7/Lab7GLFW.cpp
+++ Lab7/Lab7GLFW.cpp
@@ -2,11 +2,18 @@
 //Justyn Durnford
 //Created on 2/20/2020
 //Last Updated on 2/20/2020
-//
-//Original code @
+//https://github.com/Yaboi-Gengarboi/cs202/tree/master/Lab7
+//Original code found at
 //https://raw.githubusercontent.com/microwerx/cs202/master/LabGLFW/LabGLFW.cpp
 
 #include <GLFW/glfw3.h>
+
+//This is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main(void)
 {
@@ -27,11 +34,31 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    //Set the required callback function
+    glfwSetKeyCallback(window, key_callback);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        //Set the color of the window to gray.
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+
+        //Create the vertecies for the triangle
+        glColor3f(1.0f, 0.0f, 0.0f);    //red
+        glVertex3f(-1.0f, -1.0f, 0.0f); //left
+
+        glColor3f(0.0f, 1.0f, 0.0f);    //green
+        glVertex3f(1.0f, -1.0f, 0.0f);  //right
+
+        glColor3f(0.0f, 0.0f, 1.0f);    //blue
+        glVertex3f(0.0f, 1.0f, 0.0f);   //top
+
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
