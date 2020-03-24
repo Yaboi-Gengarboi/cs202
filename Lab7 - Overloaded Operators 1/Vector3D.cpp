5e02868ec1620bebfbe9ff002f781ff5f9f39daa
Justyn Durnford <jpdurnford@alaska.edu>
2020-03-24 12:59:26 -0700

    -Started Vector3D.cpp -Fixed a bug in Vector3D.hpp where the setters returned a double.

--- Lab7 - Overloaded Operators 1/Vector3D.cpp
+++ Lab7 - Overloaded Operators 1/Vector3D.cpp
@@ -5,4 +5,146 @@
 
 #include "Vector3D.hpp"
 // <string>
+using std::string;
+using std::to_string;
 // <iostream>
+using std::ostream;
+
+#include <cmath>
+using std::pow;
+using std::sqrt;
+
+Vector3D::Vector3D() { /* Default values are 0.0 */ }
+
+Vector3D::Vector3D(double x, double y, double z)
+{
+	_x = x;
+	_y = y;
+	_z = z;
+}
+
+Vector3D::Vector3D(double vec_arr[3])
+{
+	_x = vec_arr[0];
+	_y = vec_arr[1];
+	_z = vec_arr[2];
+}
+
+Vector3D::~Vector3D() { /* Destructor */ }
+
+double Vector3D::getX() const
+{
+	return _x;
+}
+
+double Vector3D::getY() const
+{
+	return _y;
+}
+
+double Vector3D::getZ() const
+{
+	return _z;
+}
+
+void Vector3D::setX(double x)
+{
+	_x = x;
+}
+
+void Vector3D::setY(double y)
+{
+	_y = y;
+}
+
+void Vector3D::setZ(double z)
+{
+	_z = z;
+}
+
+void Vector3D::clear()
+{
+	_x = 0;
+	_y = 0;
+	_z = 0;
+}
+
+double Vector3D::magnitude() const
+{
+	double length = 0.0;
+	length = pow(_x, 2) + pow(_y, 2) + pow(_z, 2);
+	length = sqrt(length);
+	return length;
+}
+
+void Vector3D::add(const Vector3D& vec)
+{
+	_x += vec.getX();
+	_y += vec.getY();
+	_z += vec.getZ();
+}
+
+void Vector3D::subtract(const Vector3D& vec)
+{
+	_x -= vec.getX();
+	_y -= vec.getY();
+	_z -= vec.getZ();
+}
+
+void Vector3D::scale(double scalar)
+{
+	_x *= scalar;
+	_y *= scalar;
+	_z *= scalar;
+}
+
+Vector3D Vector3D::copy() const
+{
+	Vector3D vec(_x, _y, _z);
+	return vec;
+}
+
+string Vector3D::toString() const
+{
+	string str = "<";
+	str += to_string(_x);
+	str += ", ";
+	str += to_string(_y);
+	str += ", ";
+	str += to_string(_z);
+	str += ">";
+	return str;
+}
+
+double dot_product(const Vector3D& vec1, const Vector3D& vec2)
+{
+	double d = 0.0;
+	d = ( vec1.getX() * vec2.getX() );
+	d += ( vec1.getY() * vec2.getY() );
+	d += ( vec1.getZ() * vec2.getZ() );
+	return d;
+}
+
+Vector3D cross_product(const Vector3D& vec1, const Vector3D& vec2)
+{
+	double x = ( vec1.getY() * vec2.getZ() ) - ( vec1.getZ() * vec2.getY() );
+	double y = ( vec1.getZ() * vec2.getX() ) - ( vec1.getX() * vec2.getZ() );
+	double z = ( vec1.getX() * vec2.getY() ) - ( vec1.getY() * vec2.getX() );
+	Vector3D vec(x, y, z);
+	return vec;
+}
+
+bool operator == (const Vector3D& vec1, const Vector3D& vec2)
+{
+	return ( vec1.getX() == vec2.getX() ) && ( vec1.getY() == vec2.getY() ) && ( vec1.getZ() == vec2.getZ() );
+}
+
+bool operator != (const Vector3D& vec1, const Vector3D& vec2)
+{
+	return ( vec1.getX() != vec2.getX() ) || ( vec1.getY() != vec2.getY() ) || ( vec1.getZ() != vec2.getZ() );
+}
+
+ostream& operator << (std::ostream& os, const Vector3D& vec)
+{
+	return os;
+}
--- Lab7 - Overloaded Operators 1/Vector3D.hpp
+++ Lab7 - Overloaded Operators 1/Vector3D.hpp
@@ -17,23 +17,64 @@ class Vector3D
 
 	public:
 
+	// Constructors.
 	Vector3D();
 	Vector3D(double x, double y, double z);
-	Vector3D(double vals[3]);
+	Vector3D(double vec_arr[3]);
 
+	// Destructor.
 	~Vector3D();
 
+	// Returns the specified value.
 	double getX() const;
 	double getY() const;
 	double getZ() const;
 
-	double setX(double x);
-	double setY(double y);
-	double setZ(double z);
+	// Sets the specified value to the given value.
+	void setX(double x);
+	void setY(double y);
+	void setZ(double z);
 
+	// Adds the given Vector vec onto the Vector.
+	void add(const Vector3D& vec);
+
+	// Subtracts the given Vector vec from the Vector.
+	void subtract(const Vector3D& vec);
+
+	// Multiplies the given scalar onto the Vector3D.
+	void scale(double scalar);
+
+	// Clears the values of the Vector3D.
+	void clear();
+
+	// Returns the length or magnitude of the Vector.
 	double magnitude() const;
 
+	// Returns a new copy of the Vector3D.
+	Vector3D copy() const;
+
+	// Returns an std::string representation of the Vector3D.
 	std::string toString() const;
 };
+
+// Returns the dot-product of Vector3D vec1 and vec2.
+double dot_product(const Vector3D& vec1, const Vector3D& vec2);
+
+// Returns the cross-product of Vector3D vec1 and vec2.
+Vector3D cross_product(const Vector3D& vec1, const Vector3D& vec2);
+
+// Returns the scalar projection of Vector3D vec1 onto vec2.
+double scal_proj(const Vector3D& vec1, const Vector3D& vec2);
+
+// Returns the vector projection of Vector3D vec1 onto vec2.
+Vector3D vec_proj(const Vector3D& vec1, const Vector3D& vec2);
+
+// Equality operator
+bool operator == (const Vector3D& vec1, const Vector3D& vec2);
+
+// Inequality operator
+bool operator != (const Vector3D& vec1, const Vector3D& vec2);
+
+std::ostream& operator << (std::ostream& os, const Vector3D& vec);
 
 #endif // VECTOR3D_HPP
