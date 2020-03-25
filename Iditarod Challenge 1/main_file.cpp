fb8d3223a2da323c3de037d205116a24700e6b31
Justyn Durnford <jpdurnford@alaska.edu>
2020-03-25 07:17:04 -0700

    Write a function factorial(n): Complete

--- Iditarod Challenge 1/main_file.cpp
+++ Iditarod Challenge 1/main_file.cpp
@@ -3,6 +3,14 @@
 // Created on 3/24/2020
 // Last updated on 3/24/2020
 
+int factorial(int n)
+{
+    if (n <= 1)
+        return 1;
+
+    return n * factorial(n - 1);
+}
+
 int main()
 {
 	return 0;
