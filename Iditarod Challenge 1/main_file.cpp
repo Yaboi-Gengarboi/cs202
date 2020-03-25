e736470cab526fd7a1b7430910f63121a254ff47
Justyn Durnford <jpdurnford@alaska.edu>
2020-03-25 07:26:05 -0700

    Write a function factorial_loop(n): Completed

--- Iditarod Challenge 1/main_file.cpp
+++ Iditarod Challenge 1/main_file.cpp
@@ -11,6 +11,16 @@ int factorial(int n)
     return n * factorial(n - 1);
 }
 
+int factorial_loop(int n)
+{
+    int f = 1;
+
+    for (int i = n; i > 1; --i)
+        f *= i;
+
+    return f;
+}
+
 int main()
 {
 	return 0;
